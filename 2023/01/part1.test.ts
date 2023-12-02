fa864afcfaf95a9bb6ff916535c0cd28aa838a6e
ecyrbe <ecyrbe@gmail.com>
2023-12-02 19:27:43 +0100

    add day 1

---
+++ 2023/01/part1.test.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from "vitest";
+import { load, lines, pipe, map, reduce, firstAndLast } from "../common";
+
+const DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function* extractNumbers(line: string) {
+  for (const char of line) {
+    if (DIGITS.includes(char)) {
+      yield +char;
+    }
+  }
+}
+
+function part1(input: string) {
+  return pipe(
+    lines(input),
+    map((line) =>
+      pipe(
+        extractNumbers(line),
+        firstAndLast,
+        reduce((concat, n) => +`${concat}${n}`, 0)
+      )
+    ),
+    reduce((sum, number) => sum + number, 0)
+  );
+}
+
+describe("2023/day/01/part1", () => {
+  it("should work with the example input", () => {
+    const input = `1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet`;
+    expect(part1(input)).toEqual(142);
+  });
+
+  it("should work with the puzzle input", () => {
+    const input = load(__dirname);
+    console.log(part1(input));
+    expect(part1(input)).toEqual(54390);
+  });
+});
---
+++ 2023/01/part2.test.ts
@@ -0,0 +1,75 @@
+import { describe, expect, it } from "vitest";
+import { load, lines, pipe, map, reduce, firstAndLast } from "../common";
+
+const ALPHADIGITSMAP = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+const ALPHADIGITS = Object.keys(
+  ALPHADIGITSMAP
+) as (keyof typeof ALPHADIGITSMAP)[];
+const DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function startWithAlphaDigit(line: string) {
+  for (const alphaDigit of ALPHADIGITS) {
+    if (line.startsWith(alphaDigit)) {
+      return ALPHADIGITSMAP[alphaDigit];
+    }
+  }
+  return false;
+}
+
+function* extractNumbers(line: string) {
+  while (line.length > 0) {
+    if (DIGITS.includes(line[0])) {
+      yield +line[0];
+    } else {
+      const alphaDigit = startWithAlphaDigit(line);
+      if (alphaDigit) {
+        yield alphaDigit;
+      }
+    }
+    line = line.slice(1);
+  }
+}
+
+function part2(input: string) {
+  return pipe(
+    lines(input),
+    map((line) =>
+      pipe(
+        extractNumbers(line),
+        firstAndLast,
+        reduce((concat, n) => +`${concat}${n}`, 0)
+      )
+    ),
+    reduce((sum, number) => sum + number, 0)
+  );
+}
+
+describe("2023/day/01/part2", () => {
+  it("should work with the example input", () => {
+    const input = `two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen`;
+    const result = part2(input);
+    expect(result).toEqual(281);
+  });
+
+  it("should work with the puzzle input", () => {
+    const input = load(__dirname);
+    console.log(part2(input));
+    expect(part2(input)).toEqual(54277);
+  });
+});
