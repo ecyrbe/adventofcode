3a2b493440ed11819b5b3238159aa28f16266e99
ecyrbe <ecyrbe@gmail.com>
2023-12-05 21:40:20 +0100

    improve day 4 part 2

---
+++ 2023/04/part2.test.ts
@@ -0,0 +1,63 @@
+import { drop, enumerate, lines, load, map, pluck, split } from "@utils/generators";
+import { mapFlow, pipe } from "@utils/pipe";
+import { collect, collectSet, fold, sum } from "@utils/reducers";
+import { describe, it, expect } from "vitest";
+
+const CARD_REGEX = /[:|]/g;
+const NUMBER_REGEX = /(\d+)/g;
+
+const matchAll = (regex: RegExp) => (input: string) => input.matchAll(regex);
+
+function intersect<T>(a: Set<T>, b: Set<T>) {
+  return new Set([...a].filter(item => b.has(item)));
+}
+
+function part1(input: string) {
+  const collected = pipe(
+    lines(input),
+    mapFlow(
+      split(CARD_REGEX),
+      drop(1),
+      mapFlow(
+        matchAll(NUMBER_REGEX),
+        map(match => +match[0]),
+        collectSet,
+      ),
+      fold(intersect),
+    ),
+    enumerate,
+    map(card => ({
+      id: card[0],
+      size: card[1]?.size ?? 0,
+      score: 2 ** (card[1]?.size ?? 0),
+      copies: 1,
+    })),
+    collect,
+  );
+  for (const card of collected) {
+    for (let i = card.id + 1; i <= card.id + card.size; i++) {
+      collected[i].copies += card.copies;
+    }
+  }
+  return pipe(collected, pluck("copies"), sum);
+}
+
+describe("2023/day/04/part1", () => {
+  it("should work with the example input", () => {
+    const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
+    console.log(part1(input));
+    expect(part1(input)).toEqual(30);
+  });
+
+  it("should work with the puzzle input", () => {
+    const input = load(__dirname);
+    const result = part1(input);
+    console.log(result);
+    expect(result).toEqual(5095824);
+  });
+});
