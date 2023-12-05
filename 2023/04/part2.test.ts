b5d31cf135f44d82d378e2308acf070cf77df281
ecyrbe <ecyrbe@gmail.com>
2023-12-05 22:05:55 +0100

    Update part1 to part2 in test file

--- 2023/04/part2.test.ts
+++ 2023/04/part2.test.ts
@@ -12,7 +12,7 @@ function intersect<T>(a: Set<T>, b: Set<T>) {
   return new Set([...a].filter(item => b.has(item)));
 }
 
-function part1(input: string) {
+function part2(input: string) {
   const collected = pipe(
     lines(input),
     mapFlow(
@@ -42,7 +42,7 @@ function part1(input: string) {
   return pipe(collected, pluck("copies"), sum);
 }
 
-describe("2023/day/04/part1", () => {
+describe("2023/day/04/part2", () => {
   it("should work with the example input", () => {
     const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -50,13 +50,12 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
-    console.log(part1(input));
-    expect(part1(input)).toEqual(30);
+    expect(part2(input)).toEqual(30);
   });
 
   it("should work with the puzzle input", () => {
     const input = load(__dirname);
-    const result = part1(input);
+    const result = part2(input);
     console.log(result);
     expect(result).toEqual(5095824);
   });
