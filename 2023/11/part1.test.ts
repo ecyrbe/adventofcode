4a3b2e3521bd78c1c26d3b836b23097a7a72c290
ecyrbe <ecyrbe@gmail.com>
2023-12-11 18:07:53 +0100

    add day 11 part 1

---
+++ 2023/11/part1.test.ts
@@ -0,0 +1,81 @@
+import { lines } from "@utils/generators";
+import { load } from "@utils/loader";
+import { duplicateWhen, enumerate, filter, flatMap, flatten, log, map, scan, transpose } from "@utils/operators";
+import { mapFlow, pipe } from "@utils/pipe";
+import { collect, reduce, sum } from "@utils/reducers";
+import { describe, it, expect } from "vitest";
+
+function parse(input: string) {
+  return pipe(
+    input,
+    lines,
+    mapFlow(collect),
+    duplicateWhen(stars => stars.every(star => star === ".")),
+    transpose,
+    duplicateWhen((stars: string[]) => stars.every(star => star === ".")),
+    transpose,
+    mapFlow(enumerate, collect),
+    enumerate,
+    collect,
+  );
+}
+
+function part1(input: string) {
+  //                                y        x      star
+  const skymap = parse(input) as [number, [number, string][]][];
+  const skyStars = pipe(
+    skymap,
+    map(row => [
+      row[0],
+      pipe(
+        row[1],
+        filter(column => column[1] !== "."),
+        map(column => column[0]),
+        collect,
+      ),
+    ]),
+    filter(row => (row[1] as number[]).length > 0),
+    flatMap(row =>
+      pipe(
+        row[1] as number[],
+        map(column => [row[0], column] as [number, number]),
+      ),
+    ),
+    collect,
+  );
+  const startPairs = skyStars.flatMap((star, index) =>
+    skyStars.slice(index + 1).map(other => [star, other] as [[number, number], [number, number]]),
+  );
+  return pipe(
+    startPairs,
+    map(([star1, star2]) => {
+      const [x1, y1] = star1;
+      const [x2, y2] = star2;
+      return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }),
+    sum,
+  );
+}
+
+describe("2023/day/11/part1", () => {
+  it("should work with the example input", () => {
+    const input = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`;
+    const result = part1(input);
+    expect(result).toEqual(374);
+  });
+
+  it("should work with the puzzle input", () => {
+    const input = load(__dirname);
+    const result = part1(input);
+    expect(result).toEqual(9550717);
+  });
+});
